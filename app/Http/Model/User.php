9422241fe5a532360c80aa08917b5fec356c4183
nishidat <nishidat@isao.co.jp>
2019-08-27 12:08:02 +0900

    a little bit fix.

--- app/Http/Model/User.php
+++ app/Http/Model/User.php
@@ -5,6 +5,8 @@ namespace App\Model;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
